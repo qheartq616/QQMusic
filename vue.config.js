a3975de7e4cd7b0c7537a75a1876f99c0a2e7897
qheartq616 <1359582196@qq.com>
2023-03-31 23:13:07 +0800

    更改代理配置、打开首页

--- vue.config.js
+++ vue.config.js
@@ -18,15 +18,15 @@ module.exports = defineConfig({
   /* 开发环境跨域情况的代理配置 */
   devServer: {
     // https: true, // 链接是否使用https访问 但可能在微信中不能打开（安全性问题） 不写此行配置默认 使用http协议打开
-    host: '0.0.0.0',
+    host: '127.0.0.1',
     port: '3000', 	// 指定要监听请求的端口号
     open: true, 	  // 是否运行好直接在默认浏览器打开
     hot: true,		  // 用于设置代码保存时是否进行热更新（局部刷新，不刷新整个页面）
 
     /* 接口代理器设置 可以配置多个*/
     proxy: {
-      '/pj': {
-        target: 'http://172.29.11.44:82',                   // 实际访问的服务器地址
+      '/QQMusicApi': {
+        target: 'http://localhost:3300',                   // 实际访问的服务器地址
         secure: false,                // 如果是https接口，需要配置这个参数
         changeOrigin: true,           // 是否跨域
         ws: false,                    // 是否启用websockets
