525a211105a74cd300387520e7b0a66a63425031
Y02483 <1359582196@qq.com>
2023-06-30 17:54:45 +0800

    添加滚动条

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -16,8 +16,7 @@
         <van-cell clickable center @click="playMusic(song)" value-class="custom-albumPic" :title="song.name" value="内容"
           label="描述信息" v-for="(song, index) in songList" :key="song.id">
           <template #value>
-            <img class="custom-albumPic"
-              :src="'https://y.gtimg.cn/music/photo_new/T002R300x300M000' + song.albummid + '.jpg'" />
+            <img class="custom-albumPic" :src="'https://y.gtimg.cn/music/photo_new/T002R300x300M000' + song.albummid + '.jpg'" />
             <!-- <span class="custom-value">
             </span> -->
           </template>
@@ -38,13 +37,15 @@ import { useStore } from 'vuex'
 import { useRouter, useRoute } from "vue-router";
 import { searchQuick, searchSong, songUrls, songUrl } from '@/api/QQMusicApi';
 import { Toast, Dialog, CellGroup, ConfigProvider } from 'vant';
+import Play from './Play.vue';
 
 const VanDialog = Dialog.Component;
 const thisDom = ref();
 const router = useRouter();
 
-const songList = reactive([]);
+const songList = ref([]);
 const searchKey = ref('')
+const open=ref(true)
 
 onMounted(() => {
   search();
@@ -58,8 +59,7 @@ function search() {
   param.append('key', searchKey.value || '周杰伦');
   searchSong(param).then(res => {
     if (res.result == 100) {
-      songList.length = 0
-      songList.push(...res.data.list)
+      songList.value=res.data.list
       console.log(songList)
     } else {
       Toast.fail('搜索失败，请重试！')
@@ -84,17 +84,19 @@ function playMusic(song) {
         query: { playLink: playLink },
       })
     } else {
-      Toast.fail('播放失败，可能是登陆过期了，请联系工作人员！')
+      Toast.fail('播放失败，可能是登陆过期了，请联系我！')
     }
   })
 }
 
 </script>
+
 <style lang="scss">
 .home {
   min-height: 100%;
   position: relative;
   background-color: #{$_gb_greyColor};
+  overflow: auto !important;
 
   .header {
     padding-top: 10px;
@@ -123,6 +125,11 @@ function playMusic(song) {
       max-width: 100%;
     }
 
+  }
+
+  .play {
+    // z-index: 999;
+    min-height: 100vh;
   }
 
 }</style>
